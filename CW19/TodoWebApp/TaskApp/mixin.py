4b2a11b2db7827620ccbc7dd60ba7d559b5de74a
nimagolkhanbanj <nimagolkhanban.ng@gmail.com>
2023-08-11 11:54:40 +0330

    change view in taskapp

--- CW19/TodoWebApp/TaskApp/mixin.py
+++ CW19/TodoWebApp/TaskApp/mixin.py
@@ -1,6 +1,5 @@
 from .models import Task
 from django.core.exceptions import PermissionDenied as PD
-
 class TodoOwnerRequiredMixin:
     def dispatch(self, request, *args, **kwargs):
         task = Task.objects.get(id = kwargs['task_id'])
@@ -8,3 +7,9 @@ class TodoOwnerRequiredMixin:
             raise PD
         else:
             return super().dispatch(request, *args, **kwargs)
+
+
+class AllTodoOwnerRequiredMixin:
+    def dispatch(self, request, *args, **kwargs):
+        self.Tasks = Task.objects.filter(author = request.user)
+        return super().dispatch(request, *args, **kwargs)
